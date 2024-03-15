e18130d5acd1fdead771985d77bbb1bd4bcf8db5
asharykin <aleksandr-sharykin@mail.ru>
2024-03-15 16:11:58 +0300

    committed

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -12,13 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean("aa")
+    @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
 
-//    @Bean("bb")
-//    public static Egg6 getEgg(Needle7 needle) {
-//        return new Egg6(needle);
-//    }
 }
